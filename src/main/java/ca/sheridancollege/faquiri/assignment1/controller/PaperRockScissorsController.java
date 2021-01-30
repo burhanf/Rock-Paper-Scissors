68024003a745282b49b27fd1717571de3eb64941
Burhan Faquiri <faquiri@sheridancollege.ca>
2021-01-30 17:48:48 -0500

    Simple output page created. Table is used to display outputs for the user choice, paper choice and the link to reset the game.

--- src/main/java/ca/sheridancollege/faquiri/assignment1/controller/PaperRockScissorsController.java
+++ src/main/java/ca/sheridancollege/faquiri/assignment1/controller/PaperRockScissorsController.java
@@ -1,9 +1,12 @@
 package ca.sheridancollege.faquiri.assignment1.controller;
 
+import ca.sheridancollege.faquiri.assignment1.model.PaperRock;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PaperRockScissorsController {
@@ -14,18 +17,33 @@ public class PaperRockScissorsController {
         return "input";
     }
 
-    //grab user choices
-
     //when button is clicked, call the game and determine winner from PaperRock
     @PostMapping("processWinner")
-    public String processWinner(Model model){
+    //grab user choices
+    public String processWinner(Model model, @RequestParam int choice){
+        //choice = user's choice
 
+        //create game
+        //pass in the choice of the user
+        //determine winner
+        PaperRock paperRock = new PaperRock();
+        paperRock.setUserChoice(choice);
 
+        //winner is an attribute, an string
+       // String winner = paperRock.getWinnerName();
+
+        //pass the winner into output as a model attribute
+
+        //pass in the game and get the details from there???
+        model.addAttribute("game", paperRock);
+
+        //output page
         return "output";
     }
 
-    //pass the winner into output as a model attribute
-
-    //output page
+    @GetMapping("playAgain")
+    public String reset(){
+        return ("redirect:/");
+    }
 
 }
