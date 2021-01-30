db10a43b41b09f420ad13b11bae1c52e59f79abc
Burhan Faquiri <faquiri@sheridancollege.ca>
2021-01-30 17:19:20 -0500

    Simple input page created. Contains 3 radio groups for the user to select and a play button

--- src/main/java/ca/sheridancollege/faquiri/assignment1/controller/PaperRockScissorsController.java
+++ src/main/java/ca/sheridancollege/faquiri/assignment1/controller/PaperRockScissorsController.java
@@ -1,7 +1,31 @@
 package ca.sheridancollege.faquiri.assignment1.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PaperRockScissorsController {
+    //input page
+    @RequestMapping("/")
+    public String inputPage(){
+
+        return "input";
+    }
+
+    //grab user choices
+
+    //when button is clicked, call the game and determine winner from PaperRock
+    @PostMapping("processWinner")
+    public String processWinner(Model model){
+
+
+        return "output";
+    }
+
+    //pass the winner into output as a model attribute
+
+    //output page
+
 }
