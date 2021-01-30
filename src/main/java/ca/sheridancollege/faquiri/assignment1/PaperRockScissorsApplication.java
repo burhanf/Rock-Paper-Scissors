bc6923925d86f3ae5cc2937397084aa45aa6a368
Burhan Faquiri <faquiri@sheridancollege.ca>
2021-01-30 15:01:49 -0500

    First commit, project initialization.

---
+++ src/main/java/ca/sheridancollege/faquiri/assignment1/PaperRockScissorsApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.faquiri.assignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperRockScissorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaperRockScissorsApplication.class, args);
+    }
+
+}
